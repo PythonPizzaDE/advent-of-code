42cb0e3bb480206e0e4c3423754dfb68d85e8855
Paul Frische <paul@paulfrische.xyz>
2022-12-06 16:42:19 +0100

    solve day 6

---
+++ day6/main.py
@@ -0,0 +1,23 @@
+def check_doubles(l: list) -> bool:
+    for i in range(len(l)):
+        if l[i] in l[:i]:
+            return True
+    return False
+
+
+def find_maker(inpt: str, marker_length) -> int:
+    for i in range(len(inpt) - marker_length + 1):
+        window = inpt[i: i + marker_length]
+        if not check_doubles(window):
+            return i + marker_length
+
+with open('input.txt') as file:
+    inpt = file.read()
+
+# packet markers
+print(find_maker(inpt, 4))
+
+print('='*40)
+
+# message markers
+print(find_maker(inpt, 14))
