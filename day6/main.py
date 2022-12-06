ec410e3ef9985a83046c8e262fcebd83a65dbc2e
Paul Frische <paul@paulfrische.xyz>
2022-12-06 18:36:33 +0100

    fix typo

--- day6/main.py
+++ day6/main.py
@@ -5,7 +5,7 @@ def check_doubles(l: list) -> bool:
     return False
 
 
-def find_maker(inpt: str, marker_length) -> int:
+def find_marker(inpt: str, marker_length) -> int:
     for i in range(len(inpt) - marker_length + 1):
         window = inpt[i: i + marker_length]
         if not check_doubles(window):
@@ -15,9 +15,9 @@ with open('input.txt') as file:
     inpt = file.read()
 
 # packet markers
-print(find_maker(inpt, 4))
+print(find_marker(inpt, 4))
 
 print('='*40)
 
 # message markers
-print(find_maker(inpt, 14))
+print(find_marker(inpt, 14))
